9a8d2d6e4df56f238dc1287b979ca415ae155164
milenards <contatomillenarodrigues20@gmail.com>
2022-06-13 21:35:00 -0300

    css

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>site</title>
+    <link rel="stylesheet" href="estilos.css">
+   
 </head>
 <body>
-    <h1>Olá </h1>
-    <a href = "telaLogin.php" >Cadastrar email e senha</a>
+    <h1>Olá, seja bem-vindo! </h1>
+    <a href = "telaLogin.php" class = "btncadastro" >Cadastre-se</a>
     <br>
 
-    <a href = "lista.php" > visualizar telas</a> 
+    <a href = "lista.php" class = "btnVerificarCadastro">Verificar cadastros</a> 
 </body>
 </html>
