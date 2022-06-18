46afc020e951734b2090fff268f29e173e71420e
milenards <contatomillenarodrigues20@gmail.com>
2022-06-18 16:31:45 -0300

    css

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>site</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilo.css">
    
 </head>
 <body>
-    <h1>Olá, seja bem-vindo! </h1>
+    <h1>Olá, seja bem-vindo!! </h1>
     <a href = "telaLogin.php" class = "btncadastro" >Cadastre-se</a>
     <br>
 
